835785b2773c3baf37be03397b141c5d5952e2a6
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-12-15 16:14:53 -0500

    [blog] Create blog model

---
+++ models/blog.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    header: String,
+    content: String,
+    date: Date
+});
+
+const blogPostSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+    comments: [commentSchema]
+})
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+module.exports = BlogPost;
